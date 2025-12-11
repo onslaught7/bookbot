a6243b49dedd0e155bdc990e63afb8b55d93e71c
onslaught7 <krutibashmohapatra7@gmail.com>
2025-12-11 15:57:53 +0000

    update the main method to take a file path and return the contetns of the text

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(file_path: str) -> str:
+    with open(file_path, 'r') as f:
+        return f.read()
+
+if __name__ == "__main__":
+    book_text = get_book_text('books/frankenstein.txt')
+    print(book_text)
